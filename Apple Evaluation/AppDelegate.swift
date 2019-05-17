86718ff18968a88b5a7f533376b4d8708a201afe
Keaton Burleson <keaton.burleson@me.com>
2019-05-17 11:49:16 -0500

    Initial commit

---
+++ Apple Evaluation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Apple Evaluation
+//
+//  Created by Keaton Burleson on 5/10/19.
+//  Copyright © 2019 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :osx, '10.11'
+
+target 'Apple Evaluation' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'AudioKit'
+  # Pods for Apple Evaluation
+
+end
